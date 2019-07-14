cc54ba07ddbe6f68c30f4b32f3f767ebc7927682
Tasin <tasin07khan@gmail.com>
2019-07-14 18:01:35 +0530

    Initial commit with BottomNavigation and sample grid view layout.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Barclays Mobile',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Barclays Mobile App'),
     );
   }
 }
@@ -44,68 +44,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Widget> _tabList = [
+    Container(
+      color: Colors.white,
+      child: GridView.count(
+        crossAxisCount: 3,
+        padding: EdgeInsets.all(8.0),
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 5.0,
+        children: List.generate(20, (index) {
+          return Card(
+            color: Colors.white12,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Icon(Icons.account_balance),
+                  new Text("Account")
+                ],
+              ),
+//              child: Padding(
+//                padding: const EdgeInsets.all(15.0),
+//                child: Text('Item $index'),
+//              ),
+            ),
+          );
+        }),
+      ),
+    ),
+    Container(
+      color: Colors.redAccent,
+    ),
+    Container(
+      color: Colors.lightGreen,
+    ),
+    Container(
+      color: Colors.yellowAccent,
+    )
+  ];
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+    body: DefaultTabController(
+    length: 4,
+    child: Scaffold(
+    body: TabBarView(
+    children: _tabList,
+    ),
+      bottomNavigationBar: TabBar(
+        tabs: [
+          Tab( text: 'Transfer', icon: Icon(Icons.money_off, size: 20.0) ),
+          Tab( text: 'Pay Bills', icon: Icon(Icons.account_balance_wallet, size: 20.0) ),
+          Tab( text: 'Recharge', icon: Icon(Icons.mobile_screen_share, size: 20.0) ),
+          Tab( text: 'Apply', icon: Icon(Icons.text_rotation_angleup, size: 20.0) ),
+        ],
+        onTap: (currentIndex){
+          setState(() {
+
+          });
+
+        },
+        labelStyle: TextStyle(fontSize: 13.0),
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white30,
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorColor: Colors.white,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.blue,
+    )
+    )
     );
   }
 }
